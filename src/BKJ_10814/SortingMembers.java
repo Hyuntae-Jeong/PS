3fc9962767422d7290da593d5e48157cb96322ca
hyuntae <jht000717@g.skku.edu>
2025-08-29 22:33:45 +0900

    [BKJ_10814] 나이순 정렬
    - 자바 자료 구조 중 TreeMap(key값 자동 정렬) + Value로 ArrayList 조합으로 구성
    - 실버 5

---
+++ src/BKJ_10814/SortingMembers.java
@@ -0,0 +1,57 @@
+package BKJ_10814;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N;
+    static Map<Integer, ArrayList<String>> map = new TreeMap<>();
+    static StringTokenizer token;
+    
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        initializeMembers();
+        printMembers();
+    }
+    
+    public static void initializeMembers() throws IOException {
+        Integer age; String name;
+        
+        for (int i = 0; i < N; i++) {
+            token = new StringTokenizer(br.readLine());
+            
+            age = Integer.parseInt(token.nextToken());
+            name = token.nextToken();
+            
+            map.putIfAbsent(age, new ArrayList<>());
+            map.get(age).add(name);
+        }
+    }
+    
+    public static void printMembers() {
+        StringBuilder sb = new StringBuilder();
+
+        for(Map.Entry<Integer, ArrayList<String>> entry : map.entrySet()) {
+            int age = entry.getKey();
+            
+            for (String name : entry.getValue()) {
+                sb.append(age).append(" ").append(name).append("\n");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
+
+
+
+public class SortingMembers {
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("src/BKJ_10814/input.txt"));
+        Main.main(args);
+    }
+}
