f85e8d9ba98177b415067d1d73ee2a63a0ca6f11
hyuntae <jht000717@g.skku.edu>
2025-08-31 19:53:01 +0900

    [BKJ_1764] 듣보잡
    - HashSet과 TreeSet을 필요한 의도에 따라 사용함
    - HashSet은 듣보잡을 빠르게 탐색하기 위해 사용함
    - TreeSet은 이름을 사전순으로 정렬하기 위해 사용함
    - 실버 4

---
+++ src/BKJ_1764/UnknownMan.java
@@ -0,0 +1,54 @@
+package BKJ_1764;
+
+import java.io.*;
+import java.util.*;
+
+class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N, M;
+    static Set<String> nameList;
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer token = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(token.nextToken());
+        M = Integer.parseInt(token.nextToken());
+
+        neverHeard();
+        neverSeen();
+    }
+
+    static void neverHeard() throws IOException {
+        nameList = new HashSet<>(N);
+        for (int i = 0; i < N; i++){
+            nameList.add(br.readLine());
+        }
+    }
+
+    static void neverSeen() throws IOException {
+        StringBuilder sb = new StringBuilder();
+        String name;
+        int count = 0;
+
+        Set<String> unknownList = new TreeSet<>();
+
+        for (int i = 0; i < M; i++) {
+            name = br.readLine();
+            if (nameList.contains(name)) {
+                unknownList.add(name);
+            }
+        }
+
+        sb.append(unknownList.size()).append("\n");
+        for (String names : unknownList) sb.append(names).append("\n");
+
+        System.out.println(sb);
+    }
+}
+
+
+public class UnknownMan {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("src/BKJ_1764/input.txt"));
+        Main.main(args);
+    }
+}
