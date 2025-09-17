d1df0cf4e8a98e33604e5ff6a77f2672868a458b
hyuntae <jht000717@g.skku.edu>
2025-09-17 23:48:53 +0900

    [BKJ_11724] 연결 요소의 개수
    - node[]에는 최초연결된 두개의 노드에 새로운 번호가 각자 부여된다
    - 이미 연결된 노드에 새로운 노드 하나가 연결되면 그 번호를 이어 받는다
    - leaderNode[]에는 각 번호별 대표 노드의 번호를 저장해두고 그 노드를 leader 노드로 임명한다
    - leader가 다른 그룹 두개가 만나면 합친다 (리더 노드를 동일하게 설정)
    - DFS? BFS? 문제 같지만 그냥 배열 두 개 사용한 풀이로 품
    - 실버 2

---
+++ src/BKJ_11724/Connected.java
@@ -0,0 +1,81 @@
+package BKJ_11724;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+class Main {
+    static int N, M;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int[] node, leaderNode;
+    static int leaderCount = 0;
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer token = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(token.nextToken());
+        M = Integer.parseInt(token.nextToken());
+
+        node = new int[N + 1];
+        leaderNode = new int[N + 1];
+
+        startProcess();
+        getAnswer();
+    }
+
+    static void startProcess() throws IOException {
+        for (int i = 0; i < M; i++) {
+            StringTokenizer token = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(token.nextToken());
+            int b = Integer.parseInt(token.nextToken());
+
+            if (node[a] == node[b]) {
+                if (node[a] == 0) {
+                    leaderNode[++leaderCount] = a;
+                    node[a] = leaderCount;
+                    node[b] = leaderCount;
+                }
+            } else if (node[a] == 0) {
+                node[a] = node[b];
+            } else if (node[b] == 0) {
+                node[b] = node[a];
+            } else {
+                // a != b && a != 0 && b != 0
+                // compare leader & connect
+                if (leaderNode[node[a]] != leaderNode[node[b]]) {
+                    leaderNode[node[b]] = leaderNode[node[a]];
+                }
+            }
+        }
+    }
+
+    static void getAnswer() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        boolean[] answer = new boolean[N + 1];
+        int count = 0;
+
+        for (int i = 1; i <= leaderCount; i++) {
+            if (!answer[leaderNode[i]]) {
+                answer[leaderNode[i]] = true;
+                count++;
+            }
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (node[i] == 0) count++;
+        }
+
+        sb.append(count);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
+
+public class Connected {
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("src/BKJ_11724/input.txt"));
+        Main.main(args);
+    }
+}
