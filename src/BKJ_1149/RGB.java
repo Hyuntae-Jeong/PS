8567b74d77de92ef1aaa0f70abedead8567e92d0
hyuntae <jht000717@g.skku.edu>
2025-09-22 21:32:24 +0900

    fix(BKJ_1149): 입력 & 값 구하는 로직 통합
    - 입력 받으면서 DP 진행

--- src/BKJ_1149/RGB.java
+++ src/BKJ_1149/RGB.java
@@ -12,44 +12,27 @@ class Main{
 
     public static void main(String[] args) throws IOException {
         N = Integer.parseInt(br.readLine());
-        colorInfos = new int[N][4];
-        minCost = new int[N][4];
+        minCost = new int[N+1][4];
 
         getInput();
-        goThroughAllCases();
     }
 
     static void getInput() throws IOException {
-        for (int cost, i = 0; i < N; i++) {
+        for (int cost, i = 1; i <= N; i++) {
             StringTokenizer token = new StringTokenizer(br.readLine());
 
-            cost = Integer.parseInt(token.nextToken());
-            colorInfos[i][R] = cost;
+            /* R */ cost = Integer.parseInt(token.nextToken());
+            minCost[i][R] = Math.min(minCost[i - 1][G], minCost[i - 1][B]) +  cost;
 
-            cost = Integer.parseInt(token.nextToken());
-            colorInfos[i][G] = cost;
+            /* G */ cost = Integer.parseInt(token.nextToken());
+            minCost[i][G] = Math.min(minCost[i - 1][R], minCost[i - 1][B]) + cost;
 
-            cost = Integer.parseInt(token.nextToken());;
-            colorInfos[i][B] = cost;
+            /* B */ cost = Integer.parseInt(token.nextToken());;
+            minCost[i][B] = Math.min(minCost[i - 1][R], minCost[i - 1][G]) + cost;
 
         }
-    }
 
-    static void goThroughAllCases() {
-        minCost[0][R] = colorInfos[0][R];
-        minCost[0][G] = colorInfos[0][G];
-        minCost[0][B] = colorInfos[0][B];
-
-        for (int i = 1; i < N; i++){
-            minCost[i][R] = Math.min(minCost[i - 1][G], minCost[i - 1][B]) + colorInfos[i][R];
-            minCost[i][G] = Math.min(minCost[i - 1][R], minCost[i - 1][B]) + colorInfos[i][G];
-            minCost[i][B] = Math.min(minCost[i - 1][R], minCost[i - 1][G]) + colorInfos[i][B];
-
-            /*System.out. printf("R[%d]: %d\t G[%d]: %d\t B[%d]: %d\n",
-                    i, minCost[i][R], i, minCost[i][G], i, minCost[i][B]);*/
-        }
-
-        System.out.print(Math.min(Math.min(minCost[N - 1][R], minCost[N - 1][G]), minCost[N-1][B]));
+        System.out.print(Math.min(Math.min(minCost[N][R], minCost[N][G]), minCost[N][B]));
     }
 }
 
