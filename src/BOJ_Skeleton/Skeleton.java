574186ff8a70c865f4108702e263f0f60ef2fa63
hyuntae <jht000717@g.skku.edu>
2025-10-15 21:09:11 +0900

    add: Skeleton 코드 패키지 추가

---
+++ src/BOJ_Skeleton/Skeleton.java
@@ -0,0 +1,38 @@
+package BOJ_Skeleton;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        getInput();
+    }
+
+    static int[][] inputArray;
+    static StringTokenizer token;
+
+    static void getInput() throws IOException {
+        inputArray = new int[2][N + 1];
+        token = new StringTokenizer(br.readLine());
+
+        for (int j = 0; j < N; j++) {
+            int firstNumber = Integer.parseInt(token.nextToken());
+            inputArray[0][j] = firstNumber;
+        }
+    }
+}
+
+public class Skeleton {
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("src/BOJ_Skeleton/input.txt"));
+        Main.main(args);
+    }
+}
