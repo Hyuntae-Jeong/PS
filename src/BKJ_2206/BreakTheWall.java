ed1b7dba6f60669ce06d545eb9a68f8eeb74ff5d
hyuntae <jht000717@g.skku.edu>
2025-10-21 22:21:18 +0900

    [BKJ_2206] 벽 부수고 이동하기 (동일한 기능하는 메서드 통합)
    - 그래프 이론, 그래프 탐색, 너비 우선 탐색, 격자 그래프
    - 최단거리 구하기 문제 + 벽 부수기라는 변수 존재
    
    - 많은 커밋이 보여주듯 많은 고민을 했지만 최종적인 풀이법은 다음과 같다
    1. 시작점에서의 최단거리와 끝점에서의 최단거리를 도달할 수 있는 곳 까지 구한다 (각각 arr, revArr라고 부른다)
    2. arr[N][M]에 값이 있다면 벽에 안 막히고 최단거리를 구한거다
    3. 벽을 뚫고 지나갔을 때 최단거리 구하는 방법은 벽의 4면 중 arr의 최솟값과 revArr의 최솟값을 더하는 것이다
    4. 벽에 안 막혔다면 벽 하나씩 뚫어보면서 2번에서 구한 최단거리와 비교한다
    5. 벽에 막혔다면 벽 하나씩 뚫어보면서 뚫리는 경우 중 최단거리를 구하고 뚫리는 벽이 없으면 -1을 리턴한다
    
    - 3일 동안 많은 로직 변경을 하면서 고민을 많이 한 문제이다
    - 정답 코드 안 보고 스스로 풀이법을 얻어가는 과정이 재미있었다
    - 골드 2

--- src/BKJ_2206/BreakTheWall.java
+++ src/BKJ_2206/BreakTheWall.java
@@ -19,8 +19,8 @@ class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static int N, M;
     static int[][] arr, revArr;
-    static Deque<Point> queue, wallQueue, breakableWallQueue;
-    static boolean debug = true;
+    static Deque<Point> queue, wallQueue;
+    static boolean debug = false;
     static int[] dx = {0, -1, 0, 1};
     static int[] dy = {-1, 0, 1, 0};
 
@@ -28,97 +28,73 @@ class Main {
         StringTokenizer token = new StringTokenizer(br.readLine());
         N = Integer.parseInt(token.nextToken());
         M = Integer.parseInt(token.nextToken());
-        arr = new int[N + 1][M + 1];
-        revArr = new int[N + 1][M + 1];
+        arr = new int[N + 1][M + 1];        // path
+        revArr = new int[N + 1][M + 1];     // reverse path
 
         getInput();
+        findPath(arr, 1, 1, N, M);
+        findPath(revArr, N, M, 1, 1);
 
-        if (findPathWithoutBreaking()) {
+        if(debug) {
+            printThings(arr);
+            printThings(revArr);
+        }
+
+        if (arr[N][M] != 0) {
             /**
              * @case: 벽 부수기 없이 최단거리 찾은 경우
              * @target: 최단거리 더 짧아지는 경우 (벽 1개만 부수기)
              */
-            findReversePath();
-
-            if(debug) {
-                printThings(arr);
-                printThings(revArr);
-            }
-
-            System.out.print(breakWallsOneByOne());
+            System.out.print(goThroughWall(arr[N][M]));
         } else {
             /**
              * @case: 벽에 막혀서 도착지점 도달하지 못한 경우
              * @target: 도착지점에 최단거리로 도착할 수 있는 벽 찾기
-             * @condition: 뚫리는 벽의 조건은 4면 중 arr배열 양수인 값 1개 이상과 0인 값 1개 이상 모두 존재
-             * @step
-             * 1. 뚫을 수 있는 벽 필터링 하기
-             * 2. 뚫을 수 있는 벽 없으면 -1 리턴하고, 있으면 breakableWallQueue에 저장
-             * 3. 도착 지점에서 역으로 step 구하고 revArr 배열에 저장하기
-             * 4. breakableWallQueue와 revArr를 활용해서 최단거리 되는 벽 찾기
+             * @condition: 뚫리는 벽의 조건은 4면 중 arr 양수값과 revArr 양수값 모두 존재 (시작점과 끝점에서의 최단거리가 만나는 점)
              */
+            int shortestPathAfterBlocked  = goThroughWall(N * M);
 
-            breakableWallQueue  = filterBreakableWalls();               // step 1
-            if (breakableWallQueue.isEmpty()) System.out.print("-1");   // step 2
-            else {
-                 findReversePath();                                     // step 3
-                 if (debug) printThings(revArr);
-                 breakTheWall();
-            }
+            if (shortestPathAfterBlocked == N * M) shortestPathAfterBlocked = -1;   // 벽 안 뚫리는 경우
+            System.out.print(shortestPathAfterBlocked);
         }
-        
     }
 
-    private static void breakTheWall() {
-        int finalMinStep = N * M;
-        while (!breakableWallQueue.isEmpty()) {
-            Point p = breakableWallQueue.poll();
-
-            int minStepAfterWall = N * M;
-            for (int i = 0; i < 4; i++) {
-                int stepAfterWall = getStep(p.x + dx[i], p.y + dy[i], revArr);
-                if (stepAfterWall > 0) {
-                    minStepAfterWall = Math.min(minStepAfterWall, stepAfterWall);
+    static void getInput() throws IOException {
+        wallQueue = new ArrayDeque<>();
+        for (int i = 1; i <= N; i++) {
+            String inputLine = br.readLine();
+            for (int j = 1; j <= M; j++) {
+                if (inputLine.charAt(j - 1) == '1') {
+                    arr[i][j] = -1;
+                    revArr[i][j] = -1;
+                    wallQueue.add(new Point(i, j, 0));
                 }
             }
-            finalMinStep = Math.min(finalMinStep, p.minPrevStep + 1 + minStepAfterWall);
-
-            if (debug) System.out.printf("breakable Wall: (%d, %d), minPrevStep: %d, minStepAfterWall: %d\n", p.x, p.y, p.minPrevStep, minStepAfterWall);
         }
-
-        System.out.print(finalMinStep);
     }
 
-    private static Deque<Point> filterBreakableWalls() {
-        breakableWallQueue = new ArrayDeque<>();
+    static void findPath(int[][] targetArr, int startX, int startY, int endX, int endY) {
+        queue = new ArrayDeque<>();
+        queue.add(new Point(startX, startY, 1));
+        targetArr[startX][startY] = 1;
 
-        while (!wallQueue.isEmpty()) {
-            Point p = wallQueue.poll();
-            boolean isPositive = false, isZero = false;
-            int step, minPrevStep = N * M;
-
-            for (int i = 0; i < 4; i++) {
-                step = getStep(p.x + dx[i], p.y + dy[i], arr);
-                if (step == -1) continue;
-                else if (step == 0) isZero = true;
-                else if (step > 0) {
-                    isPositive = true;
-                    minPrevStep = Math.min(minPrevStep, step);      // 최소 거리에서 벽을 뚫어야 최종적으로 최단거리가 된다
-                }
-            }
-
-            if (isZero && isPositive) {
-                p.minPrevStep = minPrevStep;
-                breakableWallQueue.push(p);
-            }
+        while (!queue.isEmpty()) {
+            Point p = queue.poll();
+            if (p.x == endX && p.y == endY) break;
+            moveNextStep(p, targetArr);
         }
-
-        return breakableWallQueue;
     }
 
-    static int breakWallsOneByOne() {
-        int shortestPath = arr[N][M];
+    static void moveNextStep(Point p, int[][] targetArr) {
+        for (int i = 0; i < 4; i++) {
+            if (getStep(p.x + dx[i], p.y + dy[i], targetArr) == 0) {
+                queue.add(new Point(p.x + dx[i], p.y + dy[i], p.step + 1));
+                targetArr[p.x + dx[i]][p.y + dy[i]] = p.step + 1;
+            }
+        }
+    }
 
+    static int goThroughWall(int shortestPath) {
         while (!wallQueue.isEmpty()) {
             Point p = wallQueue.poll();
             int minStep = N * M, minRevStep = N * M, step, revStep;
@@ -127,12 +103,14 @@ class Main {
                 step = getStep(p.x + dx[i], p.y + dy[i], arr);
                 revStep = getStep(p.x + dx[i], p.y + dy[i], revArr);
 
-                if (step != -1) minStep = Math.min(minStep, step);
-                if (revStep != -1) minRevStep = Math.min(minRevStep, revStep);
+                if (step > 0) minStep = Math.min(minStep, step);
+                if (revStep > 0) minRevStep = Math.min(minRevStep, revStep);
             }
 
-            if(debug) System.out.printf("wall (%d, %d) = min: %d, minRev: %d, step through wall: %d\n", p.x, p.y, minStep, minRevStep, minStep + 1 + minRevStep);
-            shortestPath = Math.min(shortestPath, minStep + 1 + minRevStep);
+            if (minStep != (N * M) && minRevStep != (N * M)) {
+                if (debug) System.out.printf("going through wall (%d, %d) = min: %d, minRev: %d, step through wall: %d\n", p.x, p.y, minStep, minRevStep, minStep + 1 + minRevStep);
+                shortestPath = Math.min(shortestPath, minStep + 1 + minRevStep);
+            }
         }
 
         return shortestPath;
@@ -153,62 +131,6 @@ class Main {
             System.out.println();
         }
         System.out.println();
-    }
-
-    static void getInput() throws IOException {
-        wallQueue = new ArrayDeque<>();
-        for (int i = 1; i <= N; i++) {
-            String inputLine = br.readLine();
-            for (int j = 1; j <= M; j++) {
-                if (inputLine.charAt(j - 1) == '1') {
-                    arr[i][j] = -1;
-                    revArr[i][j] = -1;
-                    wallQueue.add(new Point(i, j, 0));
-                }
-            }
-        }
-    }
-
-    static boolean findPathWithoutBreaking() {
-        queue = new ArrayDeque<>();
-        queue.add(new Point(1, 1, 1));
-        arr[1][1] = 1;
-
-        while (!queue.isEmpty()) {
-            Point p = queue.poll();
-            nextStep(p, arr);
-        }
-        
-        // 마지막 위치가 (N, M)이 아니면 벽에 막힌 것
-        return arr[N][M] != 0;
-    }
-
-    static void findReversePath() {
-        queue = new ArrayDeque<>();
-        queue.add(new Point(N, M, 1));
-        revArr[N][M] = 1;
-
-        while (!queue.isEmpty()) {
-            Point p = queue.poll();
-            nextStep(p, revArr);
-        }
-    }
-
-    static void nextStep (Point p, int[][] targetArr) {
-        for (int i = 0; i < 4; i++) {
-            if (inRangeAndCanGo(p.x + dx[i], p.y + dy[i], targetArr)) {
-                queue.add(new Point(p.x + dx[i], p.y + dy[i], p.step + 1));
-                targetArr[p.x + dx[i]][p.y + dy[i]] = p.step + 1;
-            }
-        }
-    }
-
-    static boolean inRangeAndCanGo(int x, int y, int[][] targetArr) {
-        if (x <= 0 || x > N) return false;
-        if (y <= 0 || y > M) return false;
-        if (targetArr[x][y] != 0) return false;
-
-        return true;
     }
 }
 
