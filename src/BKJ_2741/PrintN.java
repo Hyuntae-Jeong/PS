e8d3c85eec50ec0bfe0cb7b7267fda941d1f3441
hyuntae <jht000717@g.skku.edu>
2025-11-03 00:44:21 +0900

    [BKJ_2741] N 찍기
    - Class 1 전체 문제 풀기 위함
    - 브론즈 5

---
+++ src/BKJ_2741/PrintN.java
@@ -0,0 +1,29 @@
+package BKJ_2741;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= N; i++) {
+            sb.append(i).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
+
+public class PrintN {
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("src/BKJ_2741/input.txt"));
+        Main.main(args);
+    }
+}
