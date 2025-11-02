93fd399e6867065cb69bcde8ff64ce5fc35f349b
hyuntae <jht000717@g.skku.edu>
2025-11-03 00:26:30 +0900

    [BKJ_2439] 별 찍기 - 2
    - Class 1 전체 문제 풀기 위함
    - 브론즈 4

---
+++ src/BKJ_2439/Star2.java
@@ -0,0 +1,35 @@
+package BKJ_2439;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N ;
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        printStars();
+    }
+
+    static void printStars() {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N - i; j++) sb.append(" ");
+            for (int j = 1; j <= i; j++) sb.append("*");
+            sb.append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
+
+public class Star2 {
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("src/BKJ_2439/input.txt"));
+        Main.main(args);
+    }
+}
