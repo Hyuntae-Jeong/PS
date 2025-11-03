ac9b5d3eaa259c4007a1ae5d319b7ef303479d98
hyuntae <jht000717@g.skku.edu>
2025-11-03 21:14:13 +0900

    [BKJ_2884] 알람 시계
    - Class 1++ 문제
    - 브론즈 3

---
+++ src/BKJ_2884/AlarmClock.java
@@ -0,0 +1,32 @@
+package BKJ_2884;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer token = new StringTokenizer(br.readLine());
+        int hour = Integer.parseInt(token.nextToken());
+        int minute = Integer.parseInt(token.nextToken());
+
+        minute -= 45;
+        if (minute < 0) {
+            minute += 60;
+            hour -= 1;
+            if (hour < 0) {
+                hour += 24;
+            }
+        }
+        System.out.printf("%d %d", hour, minute);
+    }
+}
+
+public class AlarmClock {
+    public static void main(String[] args) throws IOException {
+        Main.main(args);
+    }
+}
