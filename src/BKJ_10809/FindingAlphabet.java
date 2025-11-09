28a69e9437cc373ed247da31de634243b819454a
hyuntae <jht000717@g.skku.edu>
2025-11-09 20:00:11 +0900

    [BKJ_10809] 알파벳 찾기
    - Class 1++
    - 브론즈 2

---
+++ src/BKJ_10809/FindingAlphabet.java
@@ -0,0 +1,28 @@
+package BKJ_10809;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        char[] word = br.readLine().toCharArray();
+        int[] letterCount = new int[27];
+        int cnt = 0;
+        for (char letter : word) {
+            cnt++;
+            int value = Integer.valueOf(letter) - 96;
+            if (letterCount[value] == 0) letterCount[value] = cnt;
+        }
+
+        for (int i = 1; i <= 26; i++) {
+            if (letterCount[i] == 0) sb.append("-1 ");
+            else sb.append(letterCount[i] - 1).append(" ");
+        }
+
+        System.out.print(sb);
+    }
+}
