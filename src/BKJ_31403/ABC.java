4e9e72e6d7f00ac4e62d3dafd47fe9e324d47c19
hyuntae <jht000717@g.skku.edu>
2025-11-09 19:19:14 +0900

    [BKJ_31403] A + B - C
    - Class 1++
    - 브론즈 4

---
+++ src/BKJ_31403/ABC.java
@@ -0,0 +1,24 @@
+package BKJ_31403;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String A, B, C;
+        A = br.readLine();
+        B = br.readLine();
+        C = br.readLine();
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(Integer.parseInt(A) + Integer.parseInt(B) - Integer.parseInt(C)).append("\n");
+        sb.append(Integer.parseInt(A + B) - Integer.parseInt(C));
+
+        System.out.print(sb);
+    }
+}
+
+public class ABC {
+}
