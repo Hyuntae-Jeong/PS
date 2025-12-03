ce315990989497f0e446060b07db4b125f4d31e7
hyuntae <jht000717@g.skku.edu>
2025-12-03 22:22:48 +0900

    fix: [Programmers] 2023 KAKAO BLIND RECRUITMENT 문제 1번
    - 날짜를 모두 일로 치환해서 사용하는 풀이
    - 한 달이 28일로 고정이라는 조건이 있기 때문에 가능한 풀이

--- src/Programmers/Kakao2023/One/PrivateInformation.java
+++ src/Programmers/Kakao2023/One/PrivateInformation.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.*;
 import java.util.stream.Collectors;
 
-class Solution {
+class Solution_Old {
 
     static int[] todayDate;
     static Map<String, Integer> termMap;
@@ -88,6 +88,50 @@ class Solution {
     }
 }
 
+class Solution {
+    static int todayDate = 0;
+    static Map<String, Integer> termMap;
+
+    public static int[] solution(String today, String[] terms, String[] privacies) {
+        convertDataType(today, terms);
+        return findExpiredPrivacy(privacies);
+    }
+
+    static void convertDataType(String today, String[] terms) {
+        // 오늘 날짜를 일수로 변경하기 (한달에 28일 고정)
+        String[] todayValue = today.split("\\.");
+        todayDate += Integer.parseInt(todayValue[0]) * 12 * 28;     // year
+        todayDate += Integer.parseInt(todayValue[1]) * 28;          // month
+        todayDate += Integer.parseInt(todayValue[2]);               // date
+
+        // 약관 유효기간 Map 형태로 변환하기
+        termMap = new HashMap<>(terms.length);
+        for (int i = 0; i < terms.length; i++) {
+            termMap.put(terms[i].split(" ")[0], Integer.valueOf(terms[i].split(" ")[1]));
+        }
+    }
+
+    static int[] findExpiredPrivacy(String[] privacies) {
+        List<Integer> answer = new ArrayList<>();
+
+        for (int i = 0; i < privacies.length; i++) {
+            String[] dateValue = privacies[i].split(" ")[0].split("\\.");
+            int expireDate = Integer.parseInt(dateValue[0]) * 12 * 28;     // year
+            expireDate += Integer.parseInt(dateValue[1]) * 28;            // month
+            expireDate += Integer.parseInt(dateValue[2]);                 // date
+
+            expireDate -= 1;
+            expireDate += 28 * termMap.get(privacies[i].split(" ")[1]);
+
+            if (todayDate > expireDate) {
+                answer.add(i + 1);
+            }
+        }
+
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
+
 public class PrivateInformation {
 
     public static void main(String[] args) throws IOException {
@@ -106,5 +150,9 @@ public class PrivateInformation {
         String[] privacies = privaciesString.split(",");
 
         int[] answer = Solution.solution(today, terms, privacies);
+
+        for (int i = 0; i < answer.length; i++) {
+            System.out.printf("%d ", answer[i]);
+        }
     }
 }
