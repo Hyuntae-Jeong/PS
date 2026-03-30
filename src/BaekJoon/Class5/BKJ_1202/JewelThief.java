65edb4f1f440a06871eff497b972630bdb6c7c69
macbook <jht000717@gmail.com>
2026-03-30 22:02:07 +0900

    feat(BKJ_1202): [보석 도둑] 가방 자료구조 결정
    - 보석은 value가 같은 경우에 무게는 어느 것을 먼저 담는지 상관없을 것으로 판단되어 내림차순으로 정렬함
    - 가방은 오름차순으로 정렬하고 빠른 탐색을 위해 Bag 배열을 생성함
    
    todo:
    - 이진탐색으로 보석을 담을 수 있는 가장 작은 가방을 찾고, 해당 가방이 이미 사용중(isFull)일 경우 link로 찾기
    - link로 찾기 위해 Bag 클래스 생성함

--- src/BaekJoon/Class5/BKJ_1202/JewelThief.java
+++ src/BaekJoon/Class5/BKJ_1202/JewelThief.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
 class Jewel {
@@ -17,10 +18,29 @@ class Jewel {
         this.weight = weight;
     }
 }
+
+class Bag {
+    int capacity;
+    boolean isFull;
+    Bag preBag;
+    Bag postBag;
+
+    Bag (int capacity) {
+        this.capacity = capacity;
+    }
+
+    Bag (int capacity, Bag preBag) {
+        this.capacity = capacity;
+        this.preBag = preBag;
+        preBag.postBag = this;
+    }
+}
+
 class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static int N, M;
     static ArrayList<Jewel> jewels;
+    static Bag[] bags;
 
     public static void main(String[] args) throws IOException {
         StringTokenizer token = new StringTokenizer(br.readLine());
@@ -28,6 +48,8 @@ class Main {
         M = Integer.parseInt(token.nextToken());
 
         getJewels();
+        getBags();
+        findPerfectBag();
     }
 
     static void getJewels() throws IOException {
@@ -44,7 +66,37 @@ class Main {
             if (j1.value != j2.value) return j2.value - j1.value;
             else return j2.weight - j1.weight;
             // todo: value가 같은 경우에는 무거운거 먼저? 가벼운거 먼저? 아니면 상관없음??
+            // -> 일단은 상관 없을 것 같아서, 무게 내림차순으로 정렬함
         });
+    }
+
+    static void getBags() throws IOException {
+        ArrayList<Integer> bagInput = new ArrayList<>(M);
+        for (int i = 0; i < M; i++) {
+            int bag = Integer.parseInt(br.readLine());
+            bagInput.add(bag);
+        }
+        bagInput.sort(Comparator.naturalOrder());
+
+        bags = new Bag[M];
+        bags[0] = new Bag(bagInput.get(0));
+        for (int i = 1; i < bagInput.size(); i++) {
+            bags[i] = new Bag(bagInput.get(i), bags[i-1]);
+        }
+
+//        Bag deleteTest = bags[2];
+//        deleteTest.isFull = true;
+//        deleteTest.preBag.postBag = deleteTest.postBag;
+//        deleteTest.postBag.preBag = deleteTest.preBag;
+
+    }
+
+    static void findPerfectBag() {
+        // 가장 값비싼 
+        for (Jewel jewel : jewels) {
+            System.out.println("value : " + jewel.value + ", weight: " + jewel.weight);
+        }
+
     }
 }
 
